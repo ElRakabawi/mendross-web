789623ccac277878e8f1d82c77df2b2c894e17d4
Muhammed <mselrakabawi@gmail.com>
2017-10-25 15:34:50 +0200

    Form posted to back-end

--- Mendross/lib/Mendross.pm
+++ Mendross/lib/Mendross.pm
@@ -7,4 +7,18 @@ get '/' => sub {
     template 'index' => { 'title' => 'Mendross' };
 };
 
+post '/draw' => sub {
+  my $gene_norm = body_parameters->get('gene');
+  my $gene = lc($gene_norm);
+  my $pog = body_parameters->get('pog');
+  my $ptg = body_parameters->get('ptg');
+
+    template 'index' => {
+      'gene' => $gene,
+      'pog' => $pog,
+      'ptg' => $ptg
+   };
+
+};
+
 true;
