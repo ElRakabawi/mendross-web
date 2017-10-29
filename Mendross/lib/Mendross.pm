c0c263e20efbc05e1a3b2d146fae429703c75e18
Muhammed <mselrakabawi@gmail.com>
2017-10-29 14:29:31 +0200

    Monohybrid finished

--- Mendross/lib/Mendross.pm
+++ Mendross/lib/Mendross.pm
@@ -1,6 +1,7 @@
 package Mendross;
 use Dancer2;
 
+
 our $VERSION = '0.1';
 
 get '/' => sub {
@@ -35,23 +36,31 @@ post '/cross' => sub {
     }
   }
 
-  my @rows = (["  ","$POG[0]", "$POG[1]"]);
-  my @rows_two =([ "$PTG[0]", $PTG[0].$POG[0], $PTG[0].$POG[1]]);
-  my @rows_three = ([ "$PTG[1]", $PTG[1].$POG[0], $PTG[1].$POG[1]]);
+  my $rows = [
+      ["  ",$POG[0], $POG[1]],
+      [ $PTG[0], $PTG[0].$POG[0], $PTG[0].$POG[1] ],
+      [ $PTG[1], $PTG[1].$POG[0], $PTG[1].$POG[1] ]
+      ];
 
 
   # END #
-
-
   #Pushing to the template 'index'
     template 'index' => {
       'genes' => $genes,
       'p_one' => $p_one,
       'p_two' => $p_two,
-      'rows' => @rows,
-      'rows_two' => @rows_two,
-      'rows_three' => @rows_three
+      'r00' => $rows->[0][0],
+      'r01' => $rows->[0][1],
+      'r02' => $rows->[0][2],
+      'r10' => $rows->[1][0],
+      'r11' => $rows->[1][1],
+      'r12' => $rows->[1][2],
+      'r20' => $rows->[2][0],
+      'r21' => $rows->[2][1],
+      'r22' => $rows->[2][2]
    };
+
+
 
 };
 
